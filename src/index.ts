f85529dddca0ab926e2a41d2cc10b81f57f56796
Jonathan Montane <jonathan.montane@algolia.com>
2019-10-23 10:42:10 +0200

    fix(exotic-encoding): reject dns resolutions with exotic encodings

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,14 @@
 import { lookup } from 'dns';
 import { URL } from 'url';
 import { promisify } from 'util';
-import { BlacklistedIPError, DNSResolveError, MalformedURLError } from './errors';
+import { Address6, Address4 } from 'ip-address';
+import {
+  BlacklistedIPError,
+  DNSResolveError,
+  MalformedURLError,
+} from './errors';
 
 const dnsLookupPromisified = promisify(lookup);
-
-const createBlacklistedIPError = <T extends Record<string, any>>(url: string, restrictedIP: string, context?: T): BlacklistedIPError<T> & T => new BlacklistedIPError(url, restrictedIP, context) as BlacklistedIPError<T> & T;
-
-interface ValidationParams<T extends Record<string, any>> {
-  url: string;
-  context?: T;
-  ipPrefixes?: string[];
-}
-
-export async function validateURL<T extends Record<string, any>>({ url, ipPrefixes = PRIVATE_IP_PREFIXES, context }: ValidationParams<T>) {
-  let parsedURL: URL;
-  try {
-    parsedURL = new URL(url);
-  } catch (e) {
-    throw new MalformedURLError(url);
-  }
-
-  const { hostname } = parsedURL;
-
-  let ip: string;
-  try {
-    const { address } = await dnsLookupPromisified(hostname);
-    // I don't believe this can happen, but just in case
-    if (typeof address !== 'string' || address.length === 0) {
-      throw new Error('Invalid address');
-    }
-    ip = address;
-  } catch (err) {
-    throw new DNSResolveError(err);
-  }
-
-  if (isRestrictedIP(ip, ipPrefixes)) {
-    throw createBlacklistedIPError<T>(url, ip, context);
-  }
-
-  return true;
-}
 
 // https://cheatsheetseries.owasp.org/cheatsheets/Server_Side_Request_Forgery_Prevention_Cheat_Sheet.html
 // https://en.wikipedia.org/wiki/Private_network
@@ -75,7 +43,70 @@ export const PRIVATE_IP_PREFIXES = [
   '::1',
 ];
 
-function isRestrictedIP(ip: string, ipPrefixes: string[] = PRIVATE_IP_PREFIXES) {
+const createBlacklistedIPError = <T extends Record<string, any>>(
+  url: string,
+  restrictedIP: string,
+  context?: T
+): BlacklistedIPError<T> & T => {
+  return new BlacklistedIPError(
+    url,
+    restrictedIP,
+    context
+  ) as BlacklistedIPError<T> & T;
+};
+
+interface ValidationParams<T extends Record<string, any>> {
+  url: string;
+  context?: T;
+  ipPrefixes?: string[];
+}
+
+export async function validateURL<T extends Record<string, any>>({
+  url,
+  ipPrefixes = PRIVATE_IP_PREFIXES,
+  context,
+}: ValidationParams<T>) {
+  let parsedURL: URL;
+  try {
+    parsedURL = new URL(url);
+  } catch (e) {
+    throw new MalformedURLError(url);
+  }
+
+  const { hostname } = parsedURL;
+
+  let ip: string;
+  try {
+    const res = await dnsLookupPromisified(hostname);
+    const { address, family } = res;
+    // I don't believe this can happen, but just in case
+    if (typeof address !== 'string' || address.length === 0) {
+      throw new Error('Invalid address');
+    }
+
+    const formattedIP =
+      family === 4 ? new Address4(address) : new Address6(address);
+
+    if (!formattedIP.isValid()) {
+      throw new Error('Invalid address');
+    }
+
+    ip = formattedIP.correctForm();
+  } catch (err) {
+    throw new DNSResolveError(err);
+  }
+
+  if (isRestrictedIP(ip, ipPrefixes)) {
+    throw createBlacklistedIPError<T>(url, ip, context);
+  }
+
+  return true;
+}
+
+function isRestrictedIP(
+  ip: string,
+  ipPrefixes: string[] = PRIVATE_IP_PREFIXES
+) {
   const sanitizedIP = ip.trim().toLowerCase();
   const matchesPrefix = (prefix: string) => sanitizedIP.startsWith(prefix);
   return ipPrefixes.some(matchesPrefix);
