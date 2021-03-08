3dc1825e22d3c645816feee8c41722fc7df87bb8
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2021-03-08 11:43:18 +0100

    fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es6: true
   },
-  extends: ["algolia/jest"],
+  extends: ["algolia", "algolia/jest"],
   globals: {
     Atomics: "readonly",
     SharedArrayBuffer: "readonly"
